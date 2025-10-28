63374c25a00c4fa8fdf9b0ca2ae26588b3f03e0e
Florian ALEXANDRE <florian.alexandre17@gmail.com>
2025-10-28 10:40:32 +0100

    qjout des limit, des offset, regle de saisie

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -6,24 +6,35 @@ const getPokemons = (app) => {
   app.get("/api/pokemons", async (req, res) => {
     if (req.query.name) {
       const name = req.query.name;
-      Pokemon.findAll({
+      const limit = parseInt(req.query.limit) || 5;
+      const offset = parseInt(req.query.offset) || 0;
+
+      if(name.length < 2) {
+        const message = `Le terme de recherche doit contenir au moins 2 caractères.`;
+        return res.status(400).json({ message: message });
+      }
+
+      Pokemon.findAndCountAll({
         where: {
           name: {
             [Op.like]: `%${name}%`,
           },
         },
+        order: [['name', 'ASC']],
+        limit: limit,
+        offset: offset,
       })
-      .then((result) => {
-        const pokemons = result.map((pokemon) => {
+       .then(({count, rows}) => {
+        const pokemons = rows.map(pokemon => {
           pokemon = pokemon.get({ plain: true });
           delete pokemon.types_string;
           return pokemon;
         });
-        const message = `Il y a ${pokemons.length} pokémon(s) qui correspondent au terme de recherche '${name}'.`;
+        const message = `Il y a ${count} pokémon(s) qui correspondent au terme de recherche '${name}'.`;
         res.json({ message: message, data: pokemons });
       });
     } else {
-      Pokemon.findAll()
+      Pokemon.findAll({order: [['name', 'ASC']] })
         .then((result) => {
           const pokemons = result.map((pokemon) => {
             pokemon = pokemon.get({ plain: true });
