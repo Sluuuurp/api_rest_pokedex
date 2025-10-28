a28469f0052124be12e7270d7a1893946918850b
Florian ALEXANDRE <florian.alexandre17@gmail.com>
2025-10-28 09:18:45 +0100

    ajout des requetes avancée, recherche par nom

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,22 +1,43 @@
 import { Pokemon } from "../db/sequelize.js";
 import pokemon from "../models/pokemon.js";
+import { Op } from "sequelize";
 
 const getPokemons = (app) => {
   app.get("/api/pokemons", async (req, res) => {
-    Pokemon.findAll()
+    if (req.query.name) {
+      const name = req.query.name;
+      Pokemon.findAll({
+        where: {
+          name: {
+            [Op.like]: `%${name}%`,
+          },
+        },
+      })
       .then((result) => {
         const pokemons = result.map((pokemon) => {
           pokemon = pokemon.get({ plain: true });
           delete pokemon.types_string;
           return pokemon;
         });
-        const message = "La liste des pokémonsa a bien été récupérée.";
+        const message = `Il y a ${pokemons.length} pokémon(s) qui correspondent au terme de recherche '${name}'.`;
         res.json({ message: message, data: pokemons });
-      })
-      .catch((error) => {
-        const message = `La liste des pokémons n'a pas pu être récupérée. Réesssayez dans quelques instants.`;
-        res.status(500).json({ message: message, data: error });
       });
+    } else {
+      Pokemon.findAll()
+        .then((result) => {
+          const pokemons = result.map((pokemon) => {
+            pokemon = pokemon.get({ plain: true });
+            delete pokemon.types_string;
+            return pokemon;
+          });
+          const message = "La liste des pokémonsa a bien été récupérée.";
+          res.json({ message: message, data: pokemons });
+        })
+        .catch((error) => {
+          const message = `La liste des pokémons n'a pas pu être récupérée. Réesssayez dans quelques instants.`;
+          res.status(500).json({ message: message, data: error });
+        });
+    }
   });
 };
 
